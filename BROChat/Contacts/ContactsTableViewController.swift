07b664dc2fabc142ed655f782f1a1aa8d68162b9
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2022-08-15 22:15:15 +0300

    change directory Contacts

--- BROChat/Contacts/ContactsTableViewController.swift
+++ BROChat/Contacts/ContactsTableViewController.swift
@@ -6,24 +6,62 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
-class ContactsTableViewController: UIViewController {
+class ContactsTableViewController: UITableViewController {
 
+    private let cellId = "ContactsCell"
+    var users = [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: .plain, target: self, action: #selector(logOut))
+        configTableView()
+        fetchUsers()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    deinit {
+        print("deinit contacts")
     }
-    */
+    
+    fileprivate func configTableView() {
+        tableView.register(ContactsTableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.separatorStyle = .none
+    }
+    
+    fileprivate func fetchUsers() {
+        FirestoreManager.shared.userManager.fetchUsers { user in
+            if user.uid != Constants.FirestoreConst.auth.currentUser?.uid ?? "" {
+                self.users.append(user)
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    @objc fileprivate func logOut() {
+        FirestoreManager.shared.logActionManager.logOut()
+    }
+    
+    // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ContactsTableViewCell
+        let currentUser = users[indexPath.row]
+        cell.userInfo = currentUser
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partnertUser = users[indexPath.row]
+        let chatVC = MessagesViewController()
+        chatVC.partnerUser = partnertUser
+        navigationController?.pushViewController(chatVC, animated: true)
+    }
 }
+
