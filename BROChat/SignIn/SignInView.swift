f2866240babd8c25c40ab281a60c116c1da8040c
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2022-08-31 14:38:36 +0300

    add textFeilds

--- BROChat/SignIn/SignInView.swift
+++ BROChat/SignIn/SignInView.swift
@@ -10,10 +10,78 @@ import UIKit
 
 class SignInView: UIView {
     
+    let closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
+        button.contentMode = .scaleToFill
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let signUpLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Оставь свои данные бро"
+        label.font = .boldSystemFont(ofSize: 30)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let avatarImage: UIImageView = {
+        let avatar = UIImageView()
+        avatar.image = UIImage(named: "camera")
+        avatar.layer.cornerRadius = 50
+        avatar.clipsToBounds = true
+        avatar.translatesAutoresizingMaskIntoConstraints = false
+        avatar.isUserInteractionEnabled = true
+        return avatar
+    }()
+
+    let nicknameTF: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your Broname"
+        textfield.layer.cornerRadius = 5
+        textfield.backgroundColor = UIColor(white: 0.9, alpha: 0.8)
+        textfield.textColor = .darkGray
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        return textfield
+    }()
+    
+    let emailTF: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Bromail"
+        textfield.layer.cornerRadius = 5
+        textfield.backgroundColor = UIColor(white: 0.9, alpha: 0.8)
+        textfield.textColor = .darkGray
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        return textfield
+    }()
+    
+    let passwordTF: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Brossword"
+        textField.layer.cornerRadius = 5
+        textField.backgroundColor = UIColor(white: 0.9, alpha: 0.8)
+        textField.textColor = .darkGray
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
-        //setConstraints()
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+        ])
+        
+        self.addSubview(nicknameTF)
     }
     
     required init?(coder: NSCoder) {
